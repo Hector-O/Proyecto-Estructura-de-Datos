4e95635d920b3b13b72f9e22de9fec91a2ae68a1
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-25 16:41:04 -0600

    Final Header

--- ProyectoFinal/proyecto.h
+++ ProyectoFinal/proyecto.h
@@ -1,7 +1,9 @@
-#ifndef __PROYECTO_H__
-#define __PROYECTO_H__
-#include<stdio.h>
-#include<stdlib.h>
+#ifndef __ARBOL_H__
+#define __ARBOL_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 struct cuentaHabiente
 {
@@ -20,15 +22,23 @@ struct arbolDeBanco
   struct arbolDeBanco *der;
 };
 
-struct arbolDeBanco *insertarArbol (struct arbolDeBanco *,struct cuentaHabiente);
+struct arbolDeBanco *insertarArbol (struct arbolDeBanco *,
+				    struct cuentaHabiente);
 void mostrarArbol (struct arbolDeBanco *);
-struct arbolDeBanco *buscarArbol (struct arbolDeBanco *,struct cuentaHabiente persona);
+struct arbolDeBanco *buscarArbol (struct arbolDeBanco *,
+				  struct cuentaHabiente persona);
 int esHoja (struct arbolDeBanco *);
-struct arbolDeBanco *padreDe (struct arbolDeBanco *,struct cuentaHabiente);
-void consultarArbol (struct arbolDeBanco *,struct cuentaHabiente);
+struct arbolDeBanco *padreDe (struct arbolDeBanco *, struct cuentaHabiente);
+void consultarArbol (struct arbolDeBanco *, struct cuentaHabiente);
 struct arbolDeBanco *mayorDe (struct arbolDeBanco *);
 struct arbolDeBanco *menorDe (struct arbolDeBanco *);
 struct arbolDeBanco *eliminarRaizDeArbol (struct arbolDeBanco *);
-struct arbolDeBanco *eliminarDeArbol (struct arbolDeBanco *, struct cuentaHabiente);
+struct arbolDeBanco *eliminarDeArbol (struct arbolDeBanco *,struct cuentaHabiente);
+int menu ();
+struct cuentaHabiente crearCH ();
+long long generarNumValido ();
+struct arbolDeBanco *buscarArbol_No_Cuenta (struct arbolDeBanco *,long long No);
+int preguntar ();
+void vacia_buffer();
 
 #endif
