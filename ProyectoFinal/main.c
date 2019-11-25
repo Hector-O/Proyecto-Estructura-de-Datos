15c7ed1655716f8f3a59cf60453ea59756694897
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-25 16:36:50 -0600

    Final Main

--- ProyectoFinal/main.c
+++ ProyectoFinal/main.c
@@ -1,47 +1,93 @@
-#include"proyecto.h"
+#include"arbol.h"
 int
 main ()
 {
   struct arbolDeBanco *unArbol = NULL;
-
-   struct cuentaHabiente personaRaiz =
-    { "Ana Morales G", 5234567891234567, 1245, 8, 2 };
-  struct cuentaHabiente persona1 =
-    { "Juan Perez Martinez", 7234567891234567, 1234, 7, 1 };
-  struct cuentaHabiente persona2 =
-    { "Sofia Maldonado Herrera", 1244567891234567, 1234, 7, 1 };
-  struct cuentaHabiente persona3 =
-    { "Menuel Aguirre Nuñez", 1234667891234567, 1234, 7, 1 };
-  struct cuentaHabiente persona4 =
-    { "Paulina Mora Carranza", 6456824598745632, 1234, 6, 2 };
-  struct cuentaHabiente persona5 =
-    { "Joseph Santos Alatorre", 8456824598745632, 1234, 6, 2 };
-  struct cuentaHabiente persona6 =
-    { "Karla Juarez Acosta", 9756824598745632, 1234, 6, 2 };
-  struct cuentaHabiente persona7 =
-    { "Salvador Montoya", 8356824598745632, 1234, 6, 2 };
-
-  unArbol = insertarArbol (unArbol, personaRaiz);
-  unArbol = insertarArbol (unArbol, persona1);
-  unArbol = insertarArbol (unArbol, persona2);
-  unArbol = insertarArbol (unArbol, persona3);
-  unArbol = insertarArbol (unArbol, persona4);
-  unArbol = insertarArbol (unArbol, persona5);
-  unArbol = insertarArbol (unArbol, persona6);
-  unArbol = insertarArbol (unArbol, persona7);
-  mostrarArbol (unArbol);
   puts ("");
+  printf("\e[2J\e[H");
+  printf ("\t\t.: S I S T E M A   B A N C A R I O   B B V A :.\n");
+  printf ("\t\t\tCONTROL DE CLIENTES\n\t\t\tSucursal 2\n");
 
-  puts ("Consulta de usuario: ");
-  consultarArbol (unArbol, persona3);
+
+  int i = 0;
+  i = menu ();
+  struct cuentaHabiente aux;
+  struct arbolDeBanco *auxi = NULL;
+  long long No;
+  int entero = 0;
+  int band = 0;
+
+  do
+   {
+      switch (i)
+  {
+  case 1:
+    printf("\e[2J\e[H");
+    aux = crearCH ();
+    unArbol = insertarArbol (unArbol, aux);
+    printf ("Su numero de cuenta es: %lld\n", aux.No_Cuenta);
+    band = preguntar ();
+    puts ("");
+    if (band)
+      {
+        printf("\e[2J\e[H");
+        i = menu ();
+      }
+    break;
+  case 2:
+    printf("\e[2J\e[H");
+    puts ("Consulta de usuario, introduzca No. de cuenta ");
+    scanf ("%lld", &No);
+    auxi = buscarArbol_No_Cuenta (unArbol, No);
+    if(auxi != NULL)
+    {
+      consultarArbol (unArbol, auxi->persona);
+    }else{
+      printf("NO se encontro\n");
+    }
+    band = preguntar ();
+    puts ("");
+    if (band)
+      {
+        printf("\e[2J\e[H");
+        i = menu ();
+      }
+    break;
+  case 3:
+    printf("\e[2J\e[H");
+    puts ("Eliminacion de usuario, introduzca el No. de cuenta");
+    scanf ("%lld", &No);
+    auxi = buscarArbol_No_Cuenta (unArbol, No);
+    if(auxi != NULL)
+    {
+      unArbol = eliminarDeArbol (unArbol, auxi->persona);
+    } else{
+      printf("NO se encontro\n");
+    }
+    band = preguntar ();
+    puts ("");
+    if (band)
+      {
+        printf("\e[2J\e[H");
+        i = menu ();
+      }
+    break;
+  case 4:
+    printf("\e[2J\e[H");
+    mostrarArbol (unArbol);
+    band = preguntar ();
+    puts ("");
+    if (band)
+      {
+        printf("\e[2J\e[H");
+        i = menu ();
+      }
+    break;
+  }
+    }
+  while (band == 1);
+
   puts ("");
-
-  unArbol = eliminarDeArbol (unArbol, persona4);
-  unArbol = eliminarDeArbol (unArbol, persona2);
-  unArbol = eliminarDeArbol (unArbol, persona1);
-  puts ("eliminando");
-  mostrarArbol (unArbol);
-
 
   return 0;
 }
